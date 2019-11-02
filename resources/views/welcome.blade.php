3bef7f397492a11c20235f3c41a7fa3c7fea8f74
Taylor Ivanoff <taylor@frankdigital.com.au>
2019-11-02 14:46:03 +1100

    Added analytics.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,17 @@
 
         <title>Batch Printing Tool</title>
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-133486425-4"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-133486425-4');
+        </script>
+        
     </head>
     <body>
         <div id="app"></div>
